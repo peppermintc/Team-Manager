a460756a2f531a302f1bcdb311a06503d5dfd88d
peppermintc <haminjp@naver.com>
2020-06-07 10:04:46 +0900

    toggle profiles

--- src/components/Content.js
+++ src/components/Content.js
@@ -53,12 +53,17 @@ const styles = (theme) => ({
 function Content(props) {
   const { classes } = props;
   
-  const [open, setOpen] = useState(false);
-
- 
-
-  const handleClick = () => {
-    setOpen(!open);
+  const [open0, setOpen0] = useState(false);
+  const handleClick0 = () => {
+    setOpen0(!open0);
+  };
+  const [open1, setOpen1] = useState(false);
+  const handleClick1 = () => {
+    setOpen1(!open1);
+  };
+  const [open2, setOpen2] = useState(false);
+  const handleClick2 = () => {
+    setOpen2(!open2);
   };
 
   let progress = 70;
@@ -97,31 +102,97 @@ function Content(props) {
       <div className={classes.contentWrapper}>
         <Typography color="textSecondary" align="center">
           <List>
-            <ListItem button style={{ display:'grid', gridTemplateColumns: '1fr 1fr 10fr 1fr 4fr 1fr', textAlign: 'center' }}>
+            <ListItem button style={{ display:'grid', gridTemplateColumns: '1fr 1fr 10fr 1fr 4fr 1fr', textAlign: 'center' }} onClick={handleClick0}>
               <div>11</div>
               <img style={{ height: '52px', border: '2.5px solid black' }} src="http://demo.themeboy.com/football-club-soccer/wp-content/uploads/sites/34/sportspress-soccer-player-4-300x300.jpg"/>
               <div style={{ textAlign: 'left', paddingLeft:'20px' }}>Mack Villarreal</div>
               <div>FW</div>
               <div>1.81m / 76.1kg</div>
-              {open ? <ExpandLess /> : <ExpandMore />}
+              {open0 ? <ExpandLess /> : <ExpandMore />}
             </ListItem>
-            <ListItem button style={{ display:'grid', gridTemplateColumns: '1fr 1fr 10fr 1fr 4fr 1fr', textAlign: 'center' }} onClick={handleClick}>
+            <Collapse in={open0} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <ListItem className={classes.nested} style={{ display:'grid', gridTemplateColumns: '1fr 2fr 1fr' }}>
+                  <img style={{ height: '200px', border: '2.5px solid black', marginLeft:'20px' }} src="http://demo.themeboy.com/football-club-soccer/wp-content/uploads/sites/34/sportspress-soccer-player-4-300x300.jpg"/>
+                  <div style={{ marginLeft:'35px', marginTop:'25px' }}>
+                    <div>Back Number: 11</div><br />
+                    <div>Name: Mack Villarreal</div><br />
+                    <div>Position: FW</div><br />
+                    <div>Height: 1.81m</div><br />
+                    <div>Weight: 76.1kg</div><br />
+                  </div>
+                  {/* CHARTJS */}
+                  <ChartPractice />                  
+                </ListItem>
+                <div style={{ paddingTop:'20px' }}>
+                  <div style={{ display:'grid', gridTemplateColumns: '1fr 5fr', paddingRight:'50px' }}>
+                    <div>Running</div>
+                    <LinearProgress variant="determinate" value={progress} style={{top:'50%'}}/>
+                    <br />
+                  </div>
+                  <div style={{ display:'grid', gridTemplateColumns: '1fr 5fr', paddingRight:'50px' }}>
+                    <div>Agility</div>
+                    <LinearProgress variant="determinate" value={progress} style={{top:'50%'}}/>
+                    <br />
+                  </div>
+                  <div style={{ display:'grid', gridTemplateColumns: '1fr 5fr', paddingRight:'50px' }}>
+                    <div>Mobility</div>
+                    <LinearProgress variant="determinate" value={progress} style={{top:'50%'}}/>
+                    <br />
+                  </div>
+                </div>
+              </List>
+            </Collapse>
+            <ListItem button style={{ display:'grid', gridTemplateColumns: '1fr 1fr 10fr 1fr 4fr 1fr', textAlign: 'center' }} onClick={handleClick1}>
               <div>11</div>
               <img style={{ height: '52px', border: '2.5px solid black' }} src="http://demo.themeboy.com/football-club-soccer/wp-content/uploads/sites/34/sportspress-soccer-player-4-300x300.jpg"/>
               <div style={{ textAlign: 'left', paddingLeft:'20px' }}>Mack Villarreal</div>
               <div>FW</div>
               <div>1.81m / 76.1kg</div>
-              {open ? <ExpandLess /> : <ExpandMore />}
+              {open1 ? <ExpandLess /> : <ExpandMore />}
             </ListItem>
-            <ListItem button style={{ display:'grid', gridTemplateColumns: '1fr 1fr 10fr 1fr 4fr 1fr', textAlign: 'center' }} onClick={handleClick}>
+            <Collapse in={open1} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <ListItem className={classes.nested} style={{ display:'grid', gridTemplateColumns: '1fr 2fr 1fr' }}>
+                  <img style={{ height: '200px', border: '2.5px solid black', marginLeft:'20px' }} src="http://demo.themeboy.com/football-club-soccer/wp-content/uploads/sites/34/sportspress-soccer-player-4-300x300.jpg"/>
+                  <div style={{ marginLeft:'35px', marginTop:'25px' }}>
+                    <div>Back Number: 11</div><br />
+                    <div>Name: Mack Villarreal</div><br />
+                    <div>Position: FW</div><br />
+                    <div>Height: 1.81m</div><br />
+                    <div>Weight: 76.1kg</div><br />
+                  </div>
+                  {/* CHARTJS */}
+                  <ChartPractice />                  
+                </ListItem>
+                <div style={{ paddingTop:'20px' }}>
+                  <div style={{ display:'grid', gridTemplateColumns: '1fr 5fr', paddingRight:'50px' }}>
+                    <div>Running</div>
+                    <LinearProgress variant="determinate" value={progress} style={{top:'50%'}}/>
+                    <br />
+                  </div>
+                  <div style={{ display:'grid', gridTemplateColumns: '1fr 5fr', paddingRight:'50px' }}>
+                    <div>Agility</div>
+                    <LinearProgress variant="determinate" value={progress} style={{top:'50%'}}/>
+                    <br />
+                  </div>
+                  <div style={{ display:'grid', gridTemplateColumns: '1fr 5fr', paddingRight:'50px' }}>
+                    <div>Mobility</div>
+                    <LinearProgress variant="determinate" value={progress} style={{top:'50%'}}/>
+                    <br />
+                  </div>
+                </div>
+              </List>
+            </Collapse>
+            <ListItem button style={{ display:'grid', gridTemplateColumns: '1fr 1fr 10fr 1fr 4fr 1fr', textAlign: 'center' }} onClick={handleClick2}>
               <div>11</div>
               <img style={{ height: '52px', border: '2.5px solid black' }} src="http://demo.themeboy.com/football-club-soccer/wp-content/uploads/sites/34/sportspress-soccer-player-4-300x300.jpg"/>
               <div style={{ textAlign: 'left', paddingLeft:'20px' }}>Mack Villarreal</div>
               <div>FW</div>
               <div>1.81m / 76.1kg</div>
-              {open ? <ExpandLess /> : <ExpandMore />}
+              {open2 ? <ExpandLess /> : <ExpandMore />}
             </ListItem>
-            <Collapse in={open} timeout="auto" unmountOnExit>
+            <Collapse in={open2} timeout="auto" unmountOnExit>
               <List component="div" disablePadding>
                 <ListItem className={classes.nested} style={{ display:'grid', gridTemplateColumns: '1fr 2fr 1fr' }}>
                   <img style={{ height: '200px', border: '2.5px solid black', marginLeft:'20px' }} src="http://demo.themeboy.com/football-club-soccer/wp-content/uploads/sites/34/sportspress-soccer-player-4-300x300.jpg"/>
