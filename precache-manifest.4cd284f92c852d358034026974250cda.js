0187ba7eb86ec5ea4e45ddf7089df5fc02aa2842
peppermintc <haminjp@naver.com>
2020-06-10 09:36:08 +0900

    Updates

---
+++ precache-manifest.4cd284f92c852d358034026974250cda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30db36a4b6b4a3da8d75243d123f6034",
+    "url": "/Team-Manager/index.html"
+  },
+  {
+    "revision": "f9ab1dc89a54f5d6136b",
+    "url": "/Team-Manager/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6cf1f1afb390584707ae",
+    "url": "/Team-Manager/static/js/2.a56d8057.chunk.js"
+  },
+  {
+    "revision": "32aeafb3e2d0a73f27822c8ed8045b88",
+    "url": "/Team-Manager/static/js/2.a56d8057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ab1dc89a54f5d6136b",
+    "url": "/Team-Manager/static/js/main.ae5886a0.chunk.js"
+  },
+  {
+    "revision": "5f78df7883b72163cb0f",
+    "url": "/Team-Manager/static/js/runtime-main.234f4313.js"
+  }
+]);
